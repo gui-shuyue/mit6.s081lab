9c0244d571b86d4519bb37161fff1d95a488ea0d
guishy3 <guishy3@mail2.sysu.edu.cn>
2026-03-23 23:21:05 +0800

    primes

--- user/pingpong.c
+++ user/pingpong.c
@@ -11,17 +11,31 @@ int main() {
     int pid = fork();
     if (pid > 0) {
         // printf("parent: ping\n");
+        close(p1[0]); // 父进程不从 p1 读，关掉
+        close(p2[1]); // 父进程不往 p2 写，关掉
+
         write(p1[1], "ping", 4);
         if (read(p2[0], buf, 4) > 0) {
             printf("%d: received pong\n", getpid());
         }
+
+        close(p1[1]); // 用完即关
+        close(p2[0]);
+
         wait(0);
     } else {
         // printf("child: pong\n");
+        close(p1[1]); // 子进程不往 p1 写，关掉
+        close(p2[0]); // 子进程不从 p2 读，关掉
+
         if (read(p1[0], buf, 4) > 0) {
             printf("%d: received ping\n", getpid());
         }
         write(p2[1], "pong", 4);
+
+        close(p1[0]); // 用完即关
+        close(p2[1]);
+
     }
     exit(0);
 }
---
+++ user/primes.c
@@ -0,0 +1,59 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+void filter(int left_pipe) {
+    int left_first;
+
+    // 读第一个数
+    if (read(left_pipe, &left_first, sizeof(int)) <= 0) {
+        close(left_pipe);
+        exit(0);
+    }
+
+    printf("prime %d\n", left_first);
+
+    int right_pipe[2];
+    pipe(right_pipe);
+
+    if (fork() == 0) {
+        close(right_pipe[1]); // 子进程不往右管道写，关掉
+        filter(right_pipe[0]);
+    } else {
+        close(right_pipe[0]); // 父进程不从右管道读，关掉
+
+        int num;
+        // 读剩下的数
+        while (read(left_pipe, &num, sizeof(int)) > 0) {
+            if (num % left_first != 0) {
+                write(right_pipe[1], &num, sizeof(int));
+            }
+        }
+
+        close(left_pipe);
+        close(right_pipe[1]);
+        wait(0);
+        exit(0);
+    }
+
+}
+
+int main() {
+    int p[2];
+    pipe(p);
+
+    if (fork() == 0) {
+        close(p[1]); // 子进程不往管道写，关掉
+        filter(p[0]);
+    } else {
+        close(p[0]); // 父进程不从管道读，关掉
+
+        for (int i = 2; i <= 35; i++) {
+            write(p[1], &i, sizeof(int));
+        }
+
+        close(p[1]);
+        wait(0);
+        exit(0);
+    }
+}
